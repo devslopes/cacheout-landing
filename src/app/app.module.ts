ac4b9ec86f4eb53c6d12dcda25c04113e3e59dfc
Jason Brewer <jason@devslopes.com>
2018-03-05 18:15:06 -0800

    finished desktop design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './home-page/navbar/navbar.component';
+import { BodyComponent } from './home-page/body/body.component';
+import { FooterComponent } from './home-page/footer/footer.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    BodyComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
